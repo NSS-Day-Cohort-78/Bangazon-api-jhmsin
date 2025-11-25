f7d5fe8d6ceb273518267743008c5abdcc6926ea
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-11-25 09:56:13 -0600

    can view my store

--- bangazonapi/views/store.py
+++ bangazonapi/views/store.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework import serializers
+from rest_framework import serializers, status
 from bangazonapi.models import Store
 from rest_framework.response import Response
-from .customer import Customer, CustomerUserSerializer, CustomerProductSerializer
+from .customer import Customer, CustomerUserSerializer
+from .product import ProductSerializer
 
 # class StoreProductsSerializer(serializers.ModelSerializer):
 #     """JSON serializer"""
@@ -18,7 +19,7 @@ class StoreOwnerSerializer(serializers.ModelSerializer):
     """JSON serializer"""
 
     user = CustomerUserSerializer(many=False)
-    products = CustomerProductSerializer(many=True)
+    products = ProductSerializer(many=True)
 
     class Meta:
         model = Customer
@@ -50,4 +51,16 @@ class Stores(ViewSet):
         stores = Store.objects.all()
 
         serializer = StoreSerializer(stores, many=True, context={"request": request})
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        """Request handler for store"""
+
+        try:
+            store = Store.objects.get(pk=pk)
+            serializer = StoreSerializer(
+                store, many=False, context={"request": request}
+            )
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Store.DoesNotExist:
+            return Response(None, status=status.HTTP_404_NOT_FOUND)

cb79cc6acedfd7dc1937fb17b2a1158dc9bb047f
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-11-25 14:50:24 -0600

    fixed some errors in the imports on product.py

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -1,17 +1,16 @@
 """View module for handling requests about products"""
 
-from rest_framework.decorators import action
-from bangazonapi.models.recommendation import Recommendation
 import base64
 from django.core.files.base import ContentFile
 from django.http import HttpResponseServerError
+from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers
-from rest_framework import status
-from bangazonapi.models import Product, Customer, ProductCategory
+from rest_framework import serializers, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.parsers import MultiPartParser, FormParser
+from bangazonapi.models import Product, Customer, ProductCategory
+from bangazonapi.models.recommendation import Recommendation
 
 
 class ProductSerializer(serializers.ModelSerializer):

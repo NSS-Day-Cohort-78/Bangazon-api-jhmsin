774ef73350326267f550a1f2669077b7b117eb42
McKenney Leavell <maleavell@gmail.com>
2025-12-01 11:45:39 -0600

    Favorited stores adding to db

--- bangazonapi/models/favorite.py
+++ bangazonapi/models/favorite.py
@@ -3,10 +3,11 @@ from django.db import models
 from .customer import Customer
 from .productcategory import ProductCategory
 from .orderproduct import OrderProduct
+from .store import Store
 from safedelete.models import SafeDeleteModel
 from safedelete.models import SOFT_DELETE
 
 class Favorite(models.Model):
 
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING,)
-    seller = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, related_name='favorited_seller')
+    seller = models.ForeignKey(Store, on_delete=models.DO_NOTHING, related_name='favorited')
